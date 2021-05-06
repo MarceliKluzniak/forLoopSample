68c8251d3d044747d34b4f5e8b7344c53b87f18b
Marceli Kluzniak <marcelikluzniak5@gmail.com>
2021-05-06 07:21:19 +0200

    sample of for loop counted from 10 to 0 one by one

---
+++ src/pl/mkluzniak/Main.java
@@ -0,0 +1,15 @@
+package pl.mkluzniak;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// for loop = executes a block of code a limited amount of times
+
+        for (int i = 10; i<=0; i--){ //loop that count from 10 to 0 one by one
+            System.out.println("Happy new year!"); //when counting ends it will be printed happy new year
+        }
+
+
+
+    }
+}
